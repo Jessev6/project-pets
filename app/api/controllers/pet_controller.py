76a9511a3f8ef18985efd166fee396cbcbfb8a58
Jesse Verbruggen <verbruggenjesse@gmail.com>
2020-11-24 15:42:32 +0100

    feat: implement pet controller

--- app/api/controllers/pet_controller.py
+++ app/api/controllers/pet_controller.py
@@ -6,25 +6,31 @@ router = APIRouter()
 
 @router.get('/api/pets')
 async def get_all_pets():
-  # TODO: implement get_all_pets
-  raise HTTPException(status_code=501)
+  return await PetRepository.get_all()
 
 @router.post('/api/pets', status_code=201)
 async def create_pet(pet: Pet):
-  # TODO: implement create_pet
-  raise HTTPException(status_code=501)
+  return await PetRepository.create(pet)
 
 @router.get('/api/pets/{id}')
 async def get_pet(id: str):
-  # TODO: implement get_pet
-  raise HTTPException(status_code=501)
+  pet = await PetRepository.get_by_id(id)
+  if pet is None:
+    raise HTTPException(status_code=404, detail="Pet not found")
+
+  return pet
 
 @router.put('/api/pets/{id}', status_code=202)
 async def update_pet(id: str, update: Pet):
-  # TODO: implement update_pet
-  raise HTTPException(status_code=501)
+  pet = await PetRepository.update(id, update)
+  if pet is None:
+    raise HTTPException(status_code=404, detail="Pet not found")
+
+  return pet
 
 @router.delete('/api/pets/{id}', status_code=204, response_class=Response)
 async def delete_pet(id: str):
-  # TODO: implement delete_pet
-  raise HTTPException(status_code=501)
+  if not await PetRepository.delete(id):
+    raise HTTPException(status_code=404, detail="Pet not found")
+
+  return

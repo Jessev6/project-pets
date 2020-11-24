9acd42231632b0881d68862c141b5e5f149738b2
Jesse Verbruggen <verbruggenjesse@gmail.com>
2020-11-24 16:22:51 +0100

    feat: implement health check for docker

--- Dockerfile
+++ Dockerfile
@@ -8,3 +8,9 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 
 ENTRYPOINT [ "uvicorn", "--host", "0.0.0.0", "--port", "80", "app.main:app" ]
+
+HEALTHCHECK \
+  --interval=5s \
+  --timeout=1s \
+  --retries=5 \
+  CMD wget http://localhost/api/healthz -q -O - > /dev/null 2>&1
--- app/api/controllers/health_controller.py
+++ app/api/controllers/health_controller.py
@@ -1,9 +1,15 @@
+from time import time
+
 from fastapi import APIRouter, HTTPException
 
 router = APIRouter()
 
+startup = time()
+delay = 15.0
 
 @router.get("/api/healthz")
 async def is_online():
-  # TODO: implement healthcheck
-  raise HTTPException(status_code=501)
+  if time() - delay < startup:
+    raise HTTPException(status_code=503, detail="App not ready")
+  
+  return "OK"
